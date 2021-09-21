406722365ceb76abb16279e88e539e2856cabc84
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-09-20 23:58:23 -0300

    adicionando app react

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,83 @@
+import React, { ChangeEvent, FormEvent, useState, useContext } from 'react'
+import {
+  Wrapper,
+  Title,
+  Form,
+  ContainerHeader,
+  InputCep,
+  ButtonPrimary,
+  ImgMapPin,
+} from './styles'
+import MapPinImg from '../../assets/map-pin.svg'
+import api from '../../services/api'
+import DataCepType from '../../types/DataCepType'
+import { CepContext } from '../../contexts/CepContext'
+import { cepIsValid } from '../../utils/cepIsValid'
+
+function Header() {
+  const [cep, setCep] = useState('')
+  const [loadingDataCep, setLoadingDataCep] = useState(false)
+  const { updateDataCep } = useContext(CepContext)
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if (!cepIsValid(cep)) {
+      alert('Por favor, insira o CEP com 8 dígitos (somente números)')
+    } else {
+      let dataCepApi: DataCepType = {}
+      setLoadingDataCep(true)
+
+      try {
+        const responseApi = await api.get(`cep/${cep}`)
+
+        setLoadingDataCep(false)
+
+        dataCepApi = responseApi.data
+
+        // Caso a API não encontre o CEP buscado, ira retornar um objeto { erro: true }
+        if (dataCepApi?.erro) {
+          alert(`CEP ${cep} não encontrado`)
+        } else {
+          updateDataCep(dataCepApi)
+        }
+      } catch {
+        alert('Erro ao realizar consulta de CEP')
+        console.error('Erro ao consultar a API')
+      }
+    }
+  }
+
+  function handleChangeInputCep(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target
+
+    setCep(value)
+  }
+
+  return (
+    <Wrapper>
+      <ContainerHeader>
+        <Title>ViaCEP</Title>
+        <Form onSubmit={handleSubmit}>
+          <div>
+            <InputCep
+              type="text"
+              placeholder="Digite um CEP (somente números) para pesquisar"
+              onChange={handleChangeInputCep}
+              value={cep}
+              maxLength={8}
+              size={8}
+              required
+            />
+            <ImgMapPin src={MapPinImg} alt="Localização" />
+          </div>
+          <ButtonPrimary type="submit" loadingDataCep={loadingDataCep}>
+            {loadingDataCep ? 'BUSCANDO' : 'BUSCAR'}
+          </ButtonPrimary>
+        </Form>
+      </ContainerHeader>
+    </Wrapper>
+  )
+}
+
+export default Header
